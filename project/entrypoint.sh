6fa3b05591ddd007ae3bf838cace79525560b61a
Alex Kressner <alexkressner@Alexs-MacBook-Air.local>
2021-08-10 11:57:28 +0200

    changed message broker to rabbitmq instead of redis

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -3,9 +3,20 @@
 echo "Waiting for mongo..."
 
 while ! nc -z mongodb 27017; do
-  sleep 0.1
+  sleep 0.25
 done
 
 echo "Mongodb started"
+
+
+
+echo "Waiting for rabbitmq..."
+
+while ! nc -z rabbitmq 5672; do
+  sleep 0.25
+done
+
+echo "rabbitmq started"
+
 
 exec "$@"
