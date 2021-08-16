f8da0c83e8c80211fcbfa914f725f11f1af10945
Alex Kressner <alexander.kressner@outlook.de>
2021-08-16 18:39:55 +0200

    change remove script

--- remove.sh
+++ remove.sh
@@ -4,6 +4,4 @@ echo "Removing stack and droplets..."
 
 eval $(docker-machine env node-1)
 docker stack rm optideploy
-for i in $(seq 1 $NUMBER_DIGITALOCEAN_NODES); do
-    docker-machine rm node-$i -y
-done
+docker-machine rm $(docker-machine ls -q) -y
