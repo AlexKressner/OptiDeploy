4fc75c8570d431c84eea96069b1d18fc4e2750df
Alex Kressner <alexander.kressner@outlook.de>
2021-08-29 23:50:56 +0200

    Add gunicorn to manage parallel uvicorn workers

--- project/app/config.py
+++ project/app/config.py
@@ -5,6 +5,14 @@ from functools import lru_cache
 import multiprocessing
 
 
+# Gunicorn config
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count()
+graceful_timeout = 120
+timeout = 120
+keepalive = 5
+
+
 class BaseConfig:
     CONNECTION: str = os.environ.get("CONNECTION")
     DATABASE_URL: str = os.environ.get("DATABASE")
@@ -12,13 +20,6 @@ class BaseConfig:
 
     CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL")
     CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND")
-
-    #gunicorn config
-    bind = "0.0.0.0:8000"
-    workers = multiprocessing.cpu_count()
-    graceful_timeout = 120
-    timeout = 120
-    keepalive = 5
 
 
 class DevelopmentConfig(BaseConfig):
