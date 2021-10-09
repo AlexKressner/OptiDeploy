81a64e2b71dd321237bc8264759d5e48c253e570
Alex Kressner <alexander.kressner@outlook.de>
2021-10-09 16:00:48 +0200

    Add Papertrail for Log-Management

--- project/app/config.py
+++ project/app/config.py
@@ -1,8 +1,11 @@
 # project/app/config.py
 
 import multiprocessing
+import os
 
-from pydantic import BaseSettings
+# I do not recommend pydantic BaseSettings here because it might cause Celery to raise [ERROR/MainProcess] pidbox
+# command error: KeyError('__signature__') error when we launch Flower
+
 
 # Gunicorn config to be loaded in docker file
 bind = "0.0.0.0:8000"
@@ -12,18 +15,15 @@ timeout = 120
 keepalive = 5
 
 
-class Settings(BaseSettings):
+class Settings():
     # mongodb settings
-    CONNECTION: str
-    DATABASE: str
+    CONNECTION: str = os.environ.get("CONNECTION")
+    DATABASE: str = os.environ.get("DATABASE")
 
     # celery settings
-    CELERY_BROKER_URL: str
-    CELERY_RESULT_BACKEND: str
-    CELERY_TASK_TIME_LIMIT: float
-
-    class Config:
-        case_sensitive = True
+    CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL")
+    CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND")
+    CELERY_TASK_TIME_LIMIT: float = os.environ.get("CELERY_TASK_TIME_LIMIT")
 
 
 settings = Settings()
